a7944f3fe3893680dc4eaa85ecab39b08b006ccb
KEITH ROGERS <asocialant@gmail.com>
2020-06-27 06:55:02 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,5 +2,5 @@ var chocolateBars =["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(array, element){
 var array1 =["one", "two"]
-var element1 =array1.unshift("zero")  
+var element1 =array1.unshift("zero")
 }addElementToBeginningOfArray(array1,element1);
