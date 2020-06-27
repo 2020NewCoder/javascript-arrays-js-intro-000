6fc522316d11578449a81e55894f42d89e2b53e7
KEITH ROGERS <asocialant@gmail.com>
2020-06-27 06:57:30 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -4,4 +4,4 @@ function addElementToBeginningOfArray(array, element){
 var array1 =["one", "two"]
 var element1 =array1.unshift("zero")
 }
-addElementToBeginningOfArray(array1,element1);
+addElementToBeginningOfArray(array1,elem
